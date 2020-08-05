d139bd5e1953898614a97bf4dcafb548c6d2551f
Abramov Ilya <abramovio1982@gmail.com>
2020-08-05 21:35:48 +0300

    Ready to deploy finally

--- Library/apps/lib/tests.py
+++ Library/apps/lib/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
-
+from django.test.client import Client
 # Create your tests here.
+
+
+class IndexTest(TestCase):
+    def test_index(self):
+        c = Client()
+        r = c.get('/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_add_author(self):
+        c = Client()
+        r = c.get('add_author/')
+        self.assertEqual(r.status_code, 200)
--- Library/settings.py
+++ Library/settings.py
@@ -26,9 +26,9 @@ sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 SECRET_KEY = 'b__@-1ft-o1vld$h&^+gr(keelg%$jls&n8l!y6jkui0)^pn!7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['mighty-beyond-10110.herokuapp.com']
+ALLOWED_HOSTS = ['mighty-beyond-10110.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -121,24 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-'''
+
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-'''
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
